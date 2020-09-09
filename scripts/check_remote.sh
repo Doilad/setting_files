f64b33e1b1664b28d7ff2cca32fdbac02cfb50de
prograsshopper <tjalwled12@gmail.com>
2020-09-09 11:10:12 +0900

    update shell script

--- scripts/check_remote.sh
+++ scripts/check_remote.sh
@@ -9,10 +9,10 @@ if [ "$HEADHASH" != "$UPSTREAMHASH" ]
 then
   echo -e ${ERROR}Not up to date with origin. Aborting.${NOCOLOR}
   echo
-  curl -X POST -H 'Content-type: application/json' --data '{"text": "New updates on remote repository"}' [github hook]
+  curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$(date): New updates on remote repository\"}" [git_hook]
   exit 0
 else
   echo -e ${FINISHED}Current branch is up to date with origin/master.${NOCOLOR}
-  curl -X POST -H 'Content-type: application/json' --data '{"text": "No updates on remote repository"}' [github hook]
+  curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$(date): No updates on remote repository\"}" [git_hook]
 
 cd
